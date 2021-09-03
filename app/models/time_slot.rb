6b1ba68c855bb37e21eaefcbadcaf91fc8f73a84
john million <jpmillion@comcast.net>
2021-09-03 17:50:22 -0400

    Set available column in time_slots to equal true before save

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,4 +1,10 @@
 class TimeSlot < ApplicationRecord
     belongs_to :coach
     belongs_to :week_day
+
+    before_save :set_available_equals_true
+    
+    def set_available_equals_true
+        self.available = true
+    end
 end
