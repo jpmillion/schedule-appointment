cfdebda2ccb94b5eace197991ce1e918ea099373
john million <jpmillion@comcast.net>
2021-09-03 20:47:26 -0400

    Seed Data for time_slots

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,172 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+TimeSlot.destroy_all
 Coach.destroy_all
 
 names = ['Christy Schumm', 'Natalia Stanton Jr.', 'Nola Murazik V', "Elyssa O'Kon", 'Dr. Geovany Keebler']
 
-names.each { |name| Coach.create(name: name) }
+coaches = names.map { |name| Coach.create(name: name) }
+
+days = WeekDay.all.map { |day| day }
+
+hr = 4
+for i in 1..17 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[0].time_slots << time_slot
+    days[0].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 3
+for i in 1..16 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[0].time_slots << time_slot
+    days[1].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 4
+for i in 1..14 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[0].time_slots << time_slot
+    days[3].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 2
+for i in 1..14 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[0].time_slots << time_slot
+    days[4].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 3
+for i in 1..4 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[1].time_slots << time_slot
+    days[1].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 6
+for i in 1..14 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[1].time_slots << time_slot
+    days[2].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 4
+for i in 1..12 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[1].time_slots << time_slot
+    days[5].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 3
+for i in 1..14 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[1].time_slots << time_slot
+    days[6].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 0
+for i in 1..4 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[2].time_slots << time_slot
+    days[0].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 3
+for i in 1..4 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[2].time_slots << time_slot
+    days[1].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 0
+for i in 1..4 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[2].time_slots << time_slot
+    days[2].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 0
+for i in 1..4 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[2].time_slots << time_slot
+    days[5].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = -1
+for i in 1..4 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[2].time_slots << time_slot
+    days[6].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 4
+for i in 1..12 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[3].time_slots << time_slot
+    days[0].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 1
+for i in 1..14 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[3].time_slots << time_slot
+    days[1].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 1
+for i in 1..10 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[3].time_slots << time_slot
+    days[2].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 3
+for i in 1..8 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[3].time_slots << time_slot
+    days[4].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 4
+for i in 1..14 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[3].time_slots << time_slot
+    days[5].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 3
+for i in 1..4 do
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[3].time_slots << time_slot
+    days[6].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
+
+hr = 2
+for i in 1..20 do
+    next if i > 14 && i < 17
+    time_slot = i % 2 == 0 ? TimeSlot.new(start: "#{hr.to_s}:30", end: "#{(hr + 1).to_s}") : TimeSlot.new(start: "#{hr.to_s}", end: "#{hr.to_s}:30")
+    coaches[4].time_slots << time_slot
+    days[3].time_slots << time_slot
+    hr += 1 if i % 2 == 0
+end
