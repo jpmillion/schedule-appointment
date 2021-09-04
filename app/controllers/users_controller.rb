3f7aa707fb2ac3960f145c9108f4b4b19c47fb10
john million <jpmillion@comcast.net>
2021-09-04 00:28:53 -0400

    Refractor create action define user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,13 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to new_user_appointment_path(@user)
+        else
+            render :new
+        end
     end
 end
 
