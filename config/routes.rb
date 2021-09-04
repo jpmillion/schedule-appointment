e97f4e024389fdeaf8a685b23493bdec1b955a46
john million <jpmillion@comcast.net>
2021-09-03 21:45:28 -0400

    Nest time_slot index route in coaches resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :coaches, only: [:index]
+  resources :coaches, only: [:index] do 
+    resources :time_slots, only: [:index]
+  end
 end
