6538e3a4243f0290db48f9801dc578148c47df23
john million <jpmillion@comcast.net>
2021-09-04 06:27:37 -0400

    Coaches index is Root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root 'coaches#index'
+
   resources :coaches, only: [:index] do 
     resources :time_slots, only: [:index]
   end
