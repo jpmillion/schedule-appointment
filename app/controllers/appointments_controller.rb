c0836d48c1588fc3178ba92cc57e51212522bb74
john million <jpmillion@comcast.net>
2021-09-03 23:19:42 -0400

    Define new and create actions in appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,11 @@
 class AppointmentsController < ApplicationController
+
+    def new
+        @user = User.find(params[:user_id])
+        @appointment = Appointment.new(user_id: @user.id) 
+    end
+
+    def create
+        
+    end
 end
