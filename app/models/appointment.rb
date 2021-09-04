7f5c3259434d2df84b37e8b87203bf998dc49bee
john million <jpmillion@comcast.net>
2021-09-04 05:30:20 -0400

    Define callback method to update time_slot availabilty

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,11 @@ class Appointment < ApplicationRecord
     belongs_to :week_day
     belongs_to :coach
     belongs_to :user
+    belongs_to :time_slot
+
+    before_destroy :update_time_slot_availability
+
+    def update_time_slot_availability
+        self.time_slot.update(available: true) 
+    end
 end
